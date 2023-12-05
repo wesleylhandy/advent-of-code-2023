f437a835d2e33b8be2b6b2e9a86e88999e378e95
Wesley Handy <whandy@costar.com>
2023-12-05 17:27:40 -0500

    Add data for day5

---
+++ day5/get-solution.js
@@ -0,0 +1,5 @@
+async function getSolution() {
+    return {};
+}
+
+module.exports.getSolution = getSolution;
--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const day1 = require('./day1/get-solution');
 const day2 = require('./day2/get-solution');
 const day3 = require('./day3/get-solution');
 const day4 = require('./day4/get-solution');
+const day5 = require('./day5/get-solution');
 
 const getSolutionByDay = new Map([
     ['1', day1.getSolution],
     ['2', day2.getSolution],
     ['3', day3.getSolution],
     ['4', day4.getSolution],
+    ['5', day5.getSolution],
 ]);
 
 async function main() {
